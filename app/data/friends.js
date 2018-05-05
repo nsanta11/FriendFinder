d10fa4ffaf255e8f1aa7cf9b8f92d00ebeaa08dd
Nicole Santarsiero <nicolesanta@Nicoles-MacBook.local>
2018-05-05 01:17:28 -0700

    added working modal feature for matches

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,5 @@
 var currentData = [
     {
-        friendName: "Toni Macaroni",
-        friendPhoto: "Link to photo",
-        scores: [1, 2, 5, 3, 4, 2, 4, 5, 1, 3]
-    },
-
-    {
         friendName: "Slim Shady",
         friendPhoto: "Link to photo2",
         scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
